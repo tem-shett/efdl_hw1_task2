f042d31f94744782a7df0391605950a24a5466f8
Artyom Shulyatev <artyomsh23@gmail.com>
2026-02-09 03:13:46 +0300

    initial

--- run_epoch.py
+++ run_epoch.py
@@ -34,7 +34,7 @@ def get_dataloader(data_mode: DataMode, batch_size: int, k: int = 1):
         return DataLoader(dataset=dataset, batch_size=batch_size, collate_fn=collate_fn_bigbrain, num_workers=4, pin_memory=True)
     elif data_mode == DataMode.ULTRA_BIG_BRAIN:
         dataset = UltraBigBrainDataset(data_path="wikitext")
-        sampler = UltraBigBrainBatchSampler(dataset, batch_size, k)
+        sampler = UltraBigBrainBatchSampler(dataset, batch_size, k=k)
         return DataLoader(dataset=dataset, batch_size=batch_size, collate_fn=collate_fn_ultrabigbrain, sampler=sampler, num_workers=4, pin_memory=True)
 
     raise NotImplementedError
